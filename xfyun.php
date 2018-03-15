76bea4728c1bde933931b63fbc0b6252ceaa0810
jusong <hanxiaomeng111@gmail.com>
2018-03-15 19:28:35 +0800

    modify xfyun.php

--- xfyun.php
+++ xfyun.php
@@ -7,18 +7,16 @@ $audioFile = '/home/jiafd/ise_cn/cn_sentence.wav';
 #$txtFile = '/home/jiafd/ise/bin/ise_cn/cn_sentence1.txt';
 #$audioFile = '/home/jiafd/ise/bin/ise_cn/cn_sentence.wav';
 
-$txtFile = '/tmp/txt';
 $txt = trim(file_get_contents($txtFile));
 $audio = file_get_contents($audioFile);
 $txt = chr(0xef).chr(0xbb).chr(0xbf).$txt;
-//exit;
 
 try {
-	    $xf = new xfyun('59cdf93b');
-		    $raw = $xf->ise($txt, $audio, xfyun::READ_SENTENCE_CN);
-			    $res = iconv('GBK', 'UTF-8', $raw);
-				    var_dump($raw, $res);
+	$xf = new xfyun('59cdf93b');
+	$raw = $xf->ise($txt, $audio, xfyun::READ_SENTENCE_CN);
+	$res = iconv('GBK', 'UTF-8', $raw);
+	var_dump($res);
 } catch (Exception $e) {
-	    var_dump($e->getCode(), $e->getMessage());
+	var_dump($e->getCode(), $e->getMessage());
 }
 var_dump($txt, strlen($txt));
